3d3904388f89c856ea8530bab1d1bb81d9254344
devahmedkhaledsaleh <dev.ahmed.khaled.saleh@gmail.com>
2022-06-06 02:13:07 +0200

    Design Page Add Post

---
+++ src/pages/posts/AddPost.tsx
@@ -0,0 +1,11 @@
+import AddPostComponent from '../../components/posts/add/AddPost'
+
+const AddPost = () => {
+  return (
+    <>
+    <AddPostComponent />
+    </>
+  )
+}
+
+export default AddPost
