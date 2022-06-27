5771a1c1af0bb425b7aa79918950a1704a5b3a49
Mostafa Magdy <mostafa.magdy621@gmail.com>
2022-06-28 00:17:56 +0200

    update post

--- src/api/adminApi.ts
+++ src/api/adminApi.ts
@@ -1,9 +1,5 @@
-import axios from 'axios'
-
+import axios from "axios";
 
 export default axios.create({
-    baseURL:`${process.env.REACT_APP_BACKEND}/admin`
-}
-
-)
-
+  baseURL: `${process.env.REACT_APP_BACKEND}admin`,
+});
