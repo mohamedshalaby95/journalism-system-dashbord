91292f7b4e912b07416f242b10aea1b18fce56ea
devahmedkhaledsaleh <dev.ahmed.khaled.saleh@gmail.com>
2022-06-28 05:11:42 +0200

    update

--- src/validation/post/postValidation.ts
+++ src/validation/post/postValidation.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const validatePostForm = function validatePostForm(post: any) {
   const schema = Joi.object({
-    title: Joi.string().min(3).max(100).required(),
+    title: Joi.string().min(5).max(100).required(),
     description: Joi.string().min(20).max(1000).required(),
     category: Joi.string(),
     subCategory: Joi.string(),
